f6be521f495df69822b65abad5344d3a4673fbc5
pengttyy <pengttyy@gmail.com>
2017-08-22 21:52:16 +0800

    feat(Streaming): Jackson 流式生成xml
    https://github.com/FasterXML/jackson-dataformat-xml/blob/master/src/test/java/com/fasterxml/jackson/dataformat/xml/stream/XmlGeneratorTest.java

--- xml/src/test/java/com/pengttyy/xml/StreamingApiTest.java
+++ xml/src/test/java/com/pengttyy/xml/StreamingApiTest.java
@@ -2,9 +2,15 @@ package com.pengttyy.xml;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.dataformat.xml.XmlFactory;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 import org.junit.Test;
 
+import javax.xml.namespace.QName;
+import java.io.StringWriter;
+
 import static com.fasterxml.jackson.core.JsonParser.Feature.ALLOW_COMMENTS;
+import static org.junit.Assert.assertEquals;
 
 public class StreamingApiTest {
 
@@ -25,5 +31,21 @@ public class StreamingApiTest {
         generator.writeEndArray();
         generator.writeEndObject();
         generator.close();
+    }
+
+    @Test
+    public void xmlStreamGenerator() throws Exception {
+        XmlFactory f = new XmlFactory();
+        StringWriter out = new StringWriter();
+        ToXmlGenerator gen = f.createGenerator(out);
+        // root name is special, need to be fed first:
+        gen.setNextName(new QName("root"));
+        gen.writeStartObject();
+        gen.writeFieldName("elem");
+        gen.writeString("value");
+        gen.writeEndObject();
+        gen.close();
+        String xml = out.toString();
+        assertEquals("<root><elem>value</elem></root>", xml);
     }
 }
